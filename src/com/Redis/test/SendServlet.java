9517f67a9ada9b101e4c481b677789cd45317dc6
jack <jack@163.com>
2019-11-08 22:36:33 +0800

    upate by eclipse

---
+++ src/com/Redis/test/SendServlet.java
@@ -0,0 +1,18 @@
+package com.Redis.test;
+
+//import javax.servlet.http.HttpServlet;
+//
+////public class SendServlet extends HttpServlet {
+////
+////}
+//
+//public class SendServlet extends HttpServlet{
+//	
+//	private static final long serialVersionUID = 1L;
+//	
+//	   
+//    public SendServlet() {
+//        
+//    }
+//	
+//}
